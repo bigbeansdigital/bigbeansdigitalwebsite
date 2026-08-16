c68c90a39a25d6fca029cd750ca4b643357271f5
BIGBEANS DIGITAL <connect.bigbeans@gmail.com>
2026-08-17 01:43:27 +0530

    new update

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Blog from "@/components/Blog";
 import Footer from "@/components/Footer";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
 
 export const metadata: Metadata = {
@@ -23,6 +24,7 @@ export default function BlogPage() {
       <Header />
       <Blog />
       <Footer />
+      <WhatsAppButton />
     </>
   );
 }
