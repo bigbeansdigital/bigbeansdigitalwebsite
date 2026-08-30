91f2e6a2a023f3f9f8f51630ce9b395ad0cf4ac0
BIGBEANS DIGITAL <connect.bigbeans@gmail.com>
2026-08-31 01:00:10 +0530

    new update | 2026-08-31 01:00

--- components/landingpage/digital-marketing-agency-kolkata.tsx
+++ components/landingpage/digital-marketing-agency-kolkata.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, type Variants } from "framer-motion";
 import {
   ArrowUpRight,
   ArrowRight,
